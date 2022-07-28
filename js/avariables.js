2eb094130b3b4795df8e83feceb192c1d88bed6e
sk281 <antoniocabralbraga1993@gmail.com>
2022-07-28 12:15:00 +0100

    thinking about how to code stuff

--- js/avariables.js
+++ js/avariables.js
@@ -1,13 +1,10 @@
 
 let ingame = 0; //change controlls from character select to fighting. to be updated
 
-
-
-
 let p1CharSelect = "redsamurai"
 let p2CharSelect = "redsamurai"
 
-let mapChoice = 3 //change it to change map
+let mapChoice = 2 //change it to change map
 let mapGameEnd = 0 //at game end changes to zero and allows to re-do map physics
 
 
@@ -15,31 +12,35 @@ let distance2Ground = 0; // to reset double jump
 let playerMinHeight = 0;
 let mapChecker = 0;
 
-//-------------------------------------------------Variables-------------------------------------------//
-
 
 let timer = 60; // game timer
 let timerID  
+
+//-------------------------------------------------Variables-------------------------------------------//
+let p1IsAnimating = 0; // tells if player1 is currently on attack animation | not implemented
+let p2IsAnimating = 0; // tells if player2 is currently on attack animation | not implemented
 
 let counterdjp1 = 0;   //doublejump player1 counter and may help with stun
 let counterdjp2 = 0;   //doublejump player2 counter and may help with stun
 
 let player1turn = 1;  // var [-1 || 1] that tells what side to shoot projectile
+let player2turn = 1;  // var [-1 || 1] that tells what side to shoot projectile | not implemented
 
-let pl1WalkR= 1; //allows player1 to walk to the right for player collision if needed
-let pl1WalkL =1;  // allows player1 to walk to the left for player collision if needed
+let pl1WalkR= 1; //allows player1 to walk to the right for player collision if needed | not implemented
+let pl1WalkL =1;  // allows player1 to walk to the left for player collision if needed | not implemented
 
-let p1gotHit = 0;     //tells if p1 got hit recently and makes him unvunerable for short duration
-let p1gotHitHard = 0; //tells if p1 got hit recently and makes him unvunerable for longer duration
+let pl2WalkR =1;// allows player2 to walk to the right not implemented
+let pl2WalkL =1;// allows player2 to walk to the left not implemented
 
-let p2gotHit = 0;   //tells if p2 got hit recently and makes him unvunerable for short duration
-let p2gotHitHard= 0;//tells if p2 got hit recently and makes him unvunerable for longer duration
+let p1gotHit = 0;     //tells if p1 got hit recently and makes him unvunerable for short duration | not implemented
+let p1gotHitHard = 0; //tells if p1 got hit recently and makes him unvunerable for longer duration | not implemented
 
-let pl2WalkR =1;// allows player2 to walk to the right
-let pl2WalkL =1;// allows player2 to walk to the left
+let p2gotHit = 0;   //tells if p2 got hit recently and makes him unvunerable for short duration | not implemented
+let p2gotHitHard= 0;//tells if p2 got hit recently and makes him unvunerable for longer duration | not implemented
 
-let unvp1 = 1; // attacks that make you unv
-let unvp2 = 2; // attacks that make you unv
+
+let unvp1 = 1; // attacks that make you unv | not implemented
+let unvp2 = 2; // attacks that make you unv | not implemented
 
 let p1move =1;  //tells when player can and cant move
 let p2move =1;  //tells when player can and cant move
